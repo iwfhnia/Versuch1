14d3bd8c6644b90c5f55fc86c0c325f21cf46a81
iwfhnia <krausesamu@gmail.com>
2025-06-20 21:20:27 +0200

    Update Arduino-Programm.ino

--- Arduino-Programm.ino
+++ Arduino-Programm.ino
@@ -6,95 +6,100 @@
 LiquidCrystal lcd(9, 8, 5, 4, 3, 2);
 
 // DHT-Sensor
-#define DHTPIN A0
-#define DHTTYPE DHT11
+#define DHTPIN      A0
+#define DHTTYPE     DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
 // Pins definieren
-const int lichtPin = A1;
-const int tasterPin = 12;
-const int piezoPin = 13;
-const int ledGruen = A3;
-const int ledGelb  = A4;
-const int ledRot   = A5;
+const int lichtPin    = A1;
+const int tasterPin   = 12;
+const int piezoPin    = 13;
+const int ledGruen    = A3;
+const int ledGelb     = A4;
+const int ledRot      = A5;
 
-unsigned long startZeit;
-const unsigned long transportDauerMin = 360; // 6h = 360min
+// Zeitsteuerung in Minuten
+unsigned long startZeitMin;
+const unsigned long transportDauerMin = 360;   // 6 Stunden = 360 Minuten
+const unsigned long messIntervallMin  = 10;    // 10 Minuten
+unsigned long letzteMessungMin        = 0;
+int messungen                        = 0;
+
+// Zuletzt gemessene Werte
+float temp       = 0.0;
+float hum        = 0.0;
+int lichtWert    = 0;
 
 void setup() {
+
   // LCD initialisieren
   lcd.begin(16, 2);
   lcd.print("System Start...");
+  delay(2000);
+  lcd.clear();
 
-  // Sensoren und Aktoren konfigurieren
+  // Sensor initialisieren
   dht.begin();
+
+  // Pin-Modi setzen
   pinMode(tasterPin, INPUT_PULLUP);
   pinMode(piezoPin, OUTPUT);
   pinMode(ledGruen, OUTPUT);
   pinMode(ledGelb, OUTPUT);
   pinMode(ledRot, OUTPUT);
-  delay(2000);
-  lcd.clear();
 
-  startZeit = millis();
-
+  // Zeitbasis setzen
+  startZeitMin      = millis() / 60000;
+  letzteMessungMin  = startZeitMin - messIntervallMin;  // erste Messung sofort
 }
 
-void loop() {  
+void loop() {
+  unsigned long jetztMin    = millis() / 60000;             // Minuten seit Start
+  unsigned long vergangenMin = jetztMin - startZeitMin;     // Minuten seit Beginn
+  long restMinuten           = transportDauerMin - vergangenMin;
 
-  // Sensorwerte lesen
-  float temp = dht.readTemperature();
-  float hum  = dht.readHumidity();
-  int lichtWert = analogRead(lichtPin);
-  bool tasterGedrueckt = digitalRead(tasterPin) == LOW;
+  // Alle 10 Minuten eine neue Messung durchführen (36x im Zeitraum)
+  if ((vergangenMin - letzteMessungMin >= messIntervallMin) && (messungen < 36)) {
+    letzteMessungMin = jetztMin;
+    messungen++;
 
-  // Restzeit berechnen
-  unsigned long vergangeneMillis = millis() - startZeit;
-  unsigned long vergangeneMinuten = vergangeneMillis / 60000;
-  long restMinuten = transportDauerMin - vergangeneMinuten;
+    // Sensorwerte einlesen
+    temp      = dht.readTemperature();
+    hum       = dht.readHumidity();
+    lichtWert = analogRead(lichtPin);
+  }
 
   // LCD-Ausgabe
   lcd.setCursor(0, 0);
-  lcd.print("T:"); lcd.print(temp, 1); lcd.print("C H:"); lcd.print(hum, 0); lcd.print("%");
+  lcd.print("T:" + String(temp,1) + "oC "); lcd.print("H:" + String(hum,0) + "%");
+
   lcd.setCursor(0, 1);
-  lcd.print("L:"); lcd.print(lichtWert); lcd.print(" Z:"); lcd.print(restMinuten > 0 ? String(restMinuten) + "min" : "Ende ");
+  lcd.print("L:" + String(lichtWert) + "lx");lcd.print(" Z:" + (restMinuten > 0 ? String(restMinuten) + "min" : "Ende "));
 
   // LED-Statuslogik
-  bool kritisch = false;
+  bool kritisch  = false;
   bool grenzwert = false;
 
-  // Temperaturbewertung
-  if (temp > 17) {
-    kritisch = true;
-  } else if (temp > 15) {
-    grenzwert = true;
-  }
+  if      (temp > 17.0)       kritisch = true;
+  else if (temp > 10.0)       grenzwert = true;
 
-  // Luftfeuchtigkeit
-  if (hum > 40) {
-    kritisch = true;
-  } else if (hum > 35) {
-    grenzwert = true;
-  }
+  if      (hum  > 40.0)       kritisch = true;
+  else if (hum  > 30.0)       grenzwert = true;
 
-  // Lichtbewertung 
-  if (lichtWert > 800) {
-    kritisch = true;
-  } else if (lichtWert > 600) {
-    grenzwert = true;
-  }
+  if      (lichtWert > 100)   kritisch = true;
+  else if (lichtWert > 70)   grenzwert = true;
 
-  // LEDs steuern
-  digitalWrite(ledGruen, !kritisch && !grenzwert);
-  digitalWrite(ledGelb, grenzwert && !kritisch);
-  digitalWrite(ledRot,  kritisch);
+  digitalWrite(ledGruen, (!kritisch && !grenzwert));
+  digitalWrite(ledGelb,  (grenzwert && !kritisch));
+  digitalWrite(ledRot,   kritisch);
 
   // Türüberwachung über Taster
+  bool tasterGedrueckt = (digitalRead(tasterPin) == LOW);
   if (!tasterGedrueckt) {
-    tone(piezoPin, 1000, 100); // leiser Piezo-Ton
+    tone(piezoPin, 1000, 100);  // Alarmsignal bei offenem Behälter
   } else {
     noTone(piezoPin);
   }
 
-  delay(1000);
+  delay(1000); 
 }
